65bd236bb2e69f12adfee297ee8316f8db2d9caf
praise <prayzstanley@gmail.com>
2025-12-05 14:58:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const loginPage = document.getElementById('loginPage');
+const loginForm = document.getElementById('loginForm');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const submit = document.getElementById('submit');
+const successMsg = document.getElementById('successMsg');
+const errorMsg = document.getElementById('errorMsg');
+const convertionPage = document.getElementById('convertionPage');
+
+const correctUsername = "makecodelit@gmail.com";
+const correctPassword = "ilovecoding123";
+debugger
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    debugger
+    const user = document.getElementById('username').value
+    const pwd = document.getElementById('password').value
+
+    if(user === correctUsername && pwd === correctPassword){
+        loginPage.style.display = "none";
+        convertionPage.style.setProperty('display', 'block', 'important');
+    }else{
+        errorMsg.style.display = "block";
+    }
+})
